66ca36d8582c7bc6d48224131e5fc39d0130fd30
Aman <itsamanali5@gmail.com>
2023-07-17 10:53:35 +0530

    order func added

--- script.js
+++ script.js
@@ -1,28 +1,37 @@
 let foodItem = undefined;
-
+let foodItemArr = undefined;
+let cardsContainer = undefined;
 // Get menu function
 function getMenu(){
     let result = fetch("https://raw.githubusercontent.com/saksham-accio/f2_contest_3/main/food.json")
-    result.then((data)=> data.json()).then((data)=>{
+    result.then(data => data.json()).then(data=>{
         foodItem=data;
-        let cards = document.getElementById("cards");
-        data.forEach(element => {
+        cardsContainer = document.getElementById("cards");
+       foodItemArr = data.map(element => {
             let card = document.createElement("div");
-            card.id="card";
+            card.className="card";
             card.innerHTML=`
-            <div id="cardImg">
+            <div class="cardImg">
                 <img src="${element.imgSrc}" alt="">
             </div>
-            <div id="cardBody">
-                <div id="c1">
+            <div class="cardBody">
+                <div class="c1">
                     <h3>${element.name}</h3>
                     <p>$${element.price}</p>
                 </div>
-                <button id="c2">+</button>
+                <button class="c2" onclick="addToMap(this)">+</button>
             </div>`;
-            cards.appendChild(card);
+            cardsContainer.appendChild(card);
+
+            return {
+                foodName:element.name,
+                cardDiv : card
+            }
             
         });
+        // console.log(foodItemArr);
+      
+      
     })
     .catch((data)=>{
         console.log("Failed",data);
@@ -34,9 +43,9 @@ function generateRandomInteger(min, max) {
   }
 //Random 3 food Items
 function addRandomItem(){
-    let itemArr = [foodItem[generateRandomInteger(1,24)],
-                   foodItem[generateRandomInteger(1,24)],
-                   foodItem[generateRandomInteger(1,24)]];
+    let itemArr = [foodItem[0],
+                   foodItem[1],
+                   foodItem[2]];
     return itemArr
 }
 
@@ -45,8 +54,8 @@ function takeOrder(){
         setTimeout(()=>{
             let randomItem = addRandomItem();
             let obj = {randomItem};
-            sec4.innerHTML= "ORDER TAKEN";
-            sec4.style.color = "black";
+           
+
             resolve(obj);
         },2500)
     });
@@ -58,6 +67,7 @@ function orderPrep(){
            let obj ={ order_status:true,paid:false};
            sec4.innerHTML= "PREPARING ORDER";
             sec4.style.color = "orange";
+            document.querySelector("#sec5 h6").style.color="green";
            return resolve(obj);       
        },1500)
         
@@ -78,6 +88,8 @@ function payOrder(obj){
 function thankyouFnc() {
     sec4.innerHTML= "Thank You";
     sec4.style.color = "red";
+    document.querySelector("#sec5 h6").innerHTML="PAID";
+    document.querySelector("#sec5 h6").style.fontSize="1.5vw";
     alert('Thank you for eating with us today!');
     console.log('Thank you for eating with us today!');
 }
@@ -85,15 +97,67 @@ function thankyouFnc() {
 async function main(){
     await getMenu();
     
-    const order = await takeOrder();
-    console.log('after 2500ms Order:', order);
+    // const order = await takeOrder();
+    // console.log('after 2500ms Order:', order);
 
-    const prepStatus = await orderPrep();
-    console.log('after 1500ms Preparation:', prepStatus);
+    // const prepStatus = await orderPrep();
+    // console.log('after 1500ms Preparation:', prepStatus);
 
-    const paymentStatus = await payOrder(prepStatus);
-    console.log('after 1000ms Payment:', paymentStatus);
-    thankyouFnc();
+    // const paymentStatus = await payOrder(prepStatus);
+    // console.log('after 1000ms Payment:', paymentStatus);
+    // thankyouFnc();
 }
 
 main();
+
+let searchBox = document.getElementById("searchVal");
+searchBox.addEventListener('input',(e)=>{
+
+    let val = e.target.value.toLowerCase();
+    // console.log(val);
+    foodItemArr.forEach((food)=>{
+        const isVisible = food.foodName.toLowerCase().includes(val);
+        food.cardDiv.classList.toggle("hide" , !isVisible)
+    })
+   
+});
+
+
+let orderMap = new Map();
+
+function addToMap(elem) {
+    let key = elem.previousElementSibling.children[0].innerText;
+    let price =  elem.previousElementSibling.children[1].innerText;
+    price=parseFloat(price.slice(1,price.length))
+    // console.log(price);
+    if(orderMap.has(key)){
+      let count = orderMap.get(key).qty;
+      count++;
+      orderMap.set(key,{qty:count,amt:price});
+    }else{
+        orderMap.set(key,{qty:1,amt:price});
+    }
+    // console.log(orderMap);
+
+    renderOrder(orderMap);
+}
+// function showOrders(){
+    
+// }
+function renderOrder(map) {
+    sec4.innerHTML= "ORDER TAKEN";
+    sec4.style.color = "black";
+    let sum =0;
+   map.forEach( (value ) => {
+        sum += value.amt*value.qty;
+      })
+      ordList.innerHTML="";
+    map.forEach( (value,key ) => {
+        let lii = document.createElement("li");
+        let tot=value.amt*value.qty;
+        lii.innerHTML = `${key} X ${value.qty} = ${tot.toFixed(2)}`
+        ordList.appendChild(lii);
+      })
+      ordSum.innerHTML=`Total Bill Amount = ${sum.toFixed(2)}$`
+    
+}
